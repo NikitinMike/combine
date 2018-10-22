b827ef479355f314ee1f972e48253e9840791a37
Mike <mike@digt.ru>
2018-10-22 14:33:42 +0300

    good job! well done.

--- main.cpp
+++ main.cpp
@@ -8,56 +8,10 @@ using namespace std;
 #include <vector>       // std::vector
 #include <array>
 
-typedef std::vector<int> VI;
+typedef std::vector<char> VI;
 
-    /*
-    comb[0]=swap(nums,0,0);
-    comb[1]=swap(nums,0,1);
-    comb[2]=swap(nums,0,2);
-
-    comb[3]=swap(comb[0],1,2);
-    comb[4]=swap(comb[1],1,2);
-    comb[5]=swap(comb[2],1,2);
-    */
-
-    /*
-    comb[ 6]=swap(comb[0],3,0);
-    comb[ 7]=swap(comb[1],3,0);
-    comb[ 8]=swap(comb[2],3,0);
-    comb[ 9]=swap(comb[3],3,0);
-    comb[10]=swap(comb[4],3,0);
-    comb[11]=swap(comb[5],3,0);
-
-    comb[12]=swap(comb[0],3,1);
-    comb[13]=swap(comb[1],3,1);
-    comb[14]=swap(comb[2],3,1);
-    comb[15]=swap(comb[3],3,1);
-    comb[16]=swap(comb[4],3,1);
-    comb[17]=swap(comb[5],3,1);
-
-    comb[18]=swap(comb[0],3,2);
-    comb[19]=swap(comb[1],3,2);
-    comb[20]=swap(comb[2],3,2);
-    comb[21]=swap(comb[3],3,2);
-    comb[22]=swap(comb[4],3,2);
-    comb[23]=swap(comb[5],3,2);
-    */
-
-struct Sum {
-    Sum() { sum = 0; }
-    void operator()(int n) { sum += n; }
-    int sum;
-};
-
-VI swap(VI vi,int a,int b){
-    swap(vi[a],vi[b]);
-    return vi;
-}
-
-void out(string s,VI nums){
-    std::cout << s;
+void out(VI nums){
     for(auto n : nums) std::cout << n << "";
-    //std::cout << '\n';
 }
 
 int factorial(int n) {
@@ -65,65 +19,41 @@ int factorial(int n) {
     return 1;
 }
 
+VI nums{'A','B','C' ,'D','E','F'}; // ,'G','H','I'
+//VI nums{4,3,2,1};
+
+// VI comb[factorial(n)];
+VI comb[1*2*3*4*5*6]; // *7*8*9
+
+VI swap(VI vi,int a,int b){
+    swap(vi[a],vi[b]);
+    return vi;
+}
+
+int combiner(int n){
+    if (n>3) {
+        int nf=combiner(n-1);
+        for(int i=0;i<nf;i++)
+            for(int j=1;j<n;j++)
+                comb[nf*j+i]=swap(comb[nf*(j-1)+i],n-j,n-j-1);
+        return nf*n;
+    }
+    comb[1]=swap(comb[0],0,1); // N=2
+    // N=3
+    comb[2]=swap(comb[0],1,2);
+    comb[3]=swap(comb[1],1,2);
+    comb[4]=swap(comb[2],0,1);
+    comb[5]=swap(comb[3],0,1);
+    return 6;
+}
+
 int main()
 {
-    // VI nums{1,2,3,4,5};
-    VI nums{4,3,2,1};
-//    VI nums{'A','B','C','D'};
-    int n=nums.size();
-    VI comb[factorial(n)];
-
-//    cout << n << " : " << sizeof(comb)/sizeof(VI) << '\n';
-    for(int i=0;i<n;i++) comb[i]=swap(nums,0,i); // 2!
-    int z=factorial(n-1)*2; // beginning of second part of combine
-    // cout << z << '\n';
-    for(int i=0;i<n;i++) { // 3!
-        comb[n+i]=swap(comb[i],1,2);
-        comb[n*2+i]=swap(comb[n+i],2,3);
-        // 4!
-        comb[z+i]=swap(comb[i],1,3);
-        comb[z+n+i]=swap(comb[n+i],1,3);
-        comb[z+n*2+i]=swap(comb[n*2+i],1,3);
-    }
-
-    /*
+    comb[0]=nums; // init as 123456789
+    std::cout << combiner(nums.size()) << endl;
     int i=0;
-    std::cout << "после:  "<< '\n';
-        for(auto c : comb) {
-            out("",c);
-            std::cout << " : " << ++i+1000;
-            std::cout << '\n'; // out(n); //
-        }
-*/
-    int k;
-    int m=factorial(k=n-1);
-    // cout << k << " : " << m << '\n';
-    if (k>9) {
-        for(int i=0;i<m;i++) {
-            comb[m*1+i]=swap(comb[i],0,1);
-            comb[m*2+i]=swap(comb[i],1,2);
-            comb[m*3+i]=swap(comb[i],2,3);
-        }
-/*
-        for(int j=1;j<=k;j++)
-            for(int i=0;i<m;i++)
-                comb[m*j+i]=swap(comb[i],k,j-1);
-                */
+    for(auto c : comb) {
+        out(c);
+        std::cout << " : " << ++i << endl;
     }
-
-    int i=0;
-    std::cout << "после:  "<< endl;
-        for(auto c : comb) {
-            out("",c);
-            std::cout << " : " << ++i+1000;
-            std::cout << endl; // out(n); //
-        }
 }
-
-void myfunction (int i) {  // function:
-  std::cout << ' ' << i;
-}
-
-struct myclass {           // function object type:
-  void operator() (int i) {std::cout << ' ' << i;}
-} myobject;
