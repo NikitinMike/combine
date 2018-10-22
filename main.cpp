0f666b1882d91c93ac756d712fd06dbd570be751
Mike <mike@digt.ru>
2018-10-22 14:51:31 +0300

    stdout reloaded and n=3 inline removed

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ using namespace std;
 
 typedef std::vector<char> VI;
 
-void out(VI nums){
-    for(auto n : nums) std::cout << n << "";
+ostream& operator<<(ostream& os, VI vi)
+{
+    for(auto n : vi) os << n << "";
+    return os;
 }
 
 int factorial(int n) {
@@ -19,11 +21,11 @@ int factorial(int n) {
     return 1;
 }
 
-VI nums{'A','B','C' ,'D','E','F'}; // ,'G','H','I'
+VI nums{'A','B','C','D'}; // ,'G','H','I'
 //VI nums{4,3,2,1};
 
 // VI comb[factorial(n)];
-VI comb[1*2*3*4*5*6]; // *7*8*9
+VI comb[1*2*3*4]; // *7*8*9
 
 VI swap(VI vi,int a,int b){
     swap(vi[a],vi[b]);
@@ -31,7 +33,7 @@ VI swap(VI vi,int a,int b){
 }
 
 int combiner(int n){
-    if (n>3) {
+    if (n>2) {
         int nf=combiner(n-1);
         for(int i=0;i<nf;i++)
             for(int j=1;j<n;j++)
@@ -39,12 +41,14 @@ int combiner(int n){
         return nf*n;
     }
     comb[1]=swap(comb[0],0,1); // N=2
+    /*
     // N=3
     comb[2]=swap(comb[0],1,2);
     comb[3]=swap(comb[1],1,2);
     comb[4]=swap(comb[2],0,1);
     comb[5]=swap(comb[3],0,1);
-    return 6;
+    */
+    return 2;
 }
 
 int main()
@@ -52,8 +56,5 @@ int main()
     comb[0]=nums; // init as 123456789
     std::cout << combiner(nums.size()) << endl;
     int i=0;
-    for(auto c : comb) {
-        out(c);
-        std::cout << " : " << ++i << endl;
-    }
+    for(auto c : comb) std::cout << c << " : " << ++i << endl;
 }
